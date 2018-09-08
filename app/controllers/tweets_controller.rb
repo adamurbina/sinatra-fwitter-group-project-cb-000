5341a40aa7c63424ffa488640ff8cd1e9e64ce67
Adam Urbina <adamurbina@gmail.com>
2018-09-08 18:26:33 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,6 +3,7 @@ class TweetsController < ApplicationController
   get "/tweets" do
     if Helpers.is_logged_in?(session)
       @user = Helpers.current_user(session)
+      @tweets = Tweet.all
       erb :'/twitter/tweets'
     else
       redirect '/login'
