432a17cbd56034d6d000ca9aed098b0ef3f5a61c
Adam Urbina <adamurbina@gmail.com>
2018-09-08 15:09:27 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
 
+  get '/login' do
+    erb :'/login'
+  end
+
+  get '/signup' do
+    erb :'/signup'
+  end
+
+  post '/signup' do
+    params.each do |key, value|
+      redirect '/signup' if value.empty?
+    end
+
+    user = User.create(username: params[:username], email: params[:email], password: params[:password])
+
+    erb :'/twitter/tweets'
+
+  end
+
+  post '/login' do
+    @user = User.find_by(username: params[:username], password: params[:password], )
+
+    if @user
+      redirect :'/twitter/tweets'
+    end
+  end
 
 end
