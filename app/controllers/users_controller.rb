3084323681fc5e6d0450dc21e36d9f28ef2d9010
Adam Urbina <adamurbina@gmail.com>
2018-09-08 15:21:42 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
 
     user = User.create(username: params[:username], email: params[:email], password: params[:password])
 
-    erb :'/twitter/tweets'
+    redi :'/twitter/tweets'
 
   end
 
