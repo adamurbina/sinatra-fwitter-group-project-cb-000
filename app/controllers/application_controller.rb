db98884c7987e62e477e174ee48f741457794b49
Adam Urbina <adamurbina@gmail.com>
2018-09-08 15:09:12 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,24 +11,4 @@ class ApplicationController < Sinatra::Base
     erb :'/layout'
   end
 
-  get '/login' do
-    erb :'/login'
-  end
-
-  get '/signup' do
-    erb :'/signup'
-  end
-
-  post '/signup' do
-    params.each do |key, value|
-      redirect '/signup' if value.empty?
-    end
-
-    user = User.create(username: params[:username], email: params[:email], password: params[:password])
-
-    erb :'/twitter/tweets'
-
-  end
-
-
 end
