a325b77c7293a9406a7711102b9273da8182e7d8
Adam Urbina <adamurbina@gmail.com>
2018-09-08 16:17:28 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
     user = User.create(username: params[:username], email: params[:email], password: params[:password])
 
     session[:user_id] = user.id
-    binding.pry
+
     erb :'/twitter/tweets'
 
   end
